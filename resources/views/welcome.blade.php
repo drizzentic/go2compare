623c2f2a94623cba1a86bdc1d555b17c58d07ac9
maikofelix89 <maikofelix89@gmail.com>
2015-08-28 13:54:38 +0300

    changed design a bit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -14,7 +14,7 @@
           <h1 id="intro">got2compare</h1>
            <h4>Insurance</h4>
           <div id="intro-message">
-          <h5 id="message1">Trusted Car,health and life insurance</h5>
+          <h5 id="message1">Get to compare insurance covers from 47+ insurance companies</h5>
          
           </div>
          
